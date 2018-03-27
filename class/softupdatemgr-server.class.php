a99b51879f0b83b19d9c44a42b6a4803771138b3
jonyhayama <jonathan.hayama@gmail.com>
2018-03-27 09:24:01 -0400

    Added hooks for license key validation

--- class/softupdatemgr-server.class.php
+++ class/softupdatemgr-server.class.php
@@ -1,12 +1,32 @@
 <?php
 require_once SOFTUPDATEMGR_DIR_PATH . 'lib/wp-update-server/loader.php';
 class softupdatemgr_server extends Wpup_UpdateServer{
-    
+
     protected function generateDownloadUrl(Wpup_Package $package) {
         $query = array(
             'softupdatemgr_action' => 'download',
             'softupdatemgr_slug' => $package->slug,
         );
+        if( softupdatemgr()->isAuthenticatedRequest() ){
+            $license = get_query_var('softupdatemgr_license');
+            if( $license ){
+                $query['softupdatemgr_license'] = $license;
+            }
+        }
         return self::addQueryArg( $query, $this->serverUrl );
     }
+
+    //Secure Download URL
+    protected function filterMetadata($meta, $request) {
+		$meta = parent::filterMetadata($meta, $request);
+        if( !softupdatemgr()->isAuthenticatedRequest() ){
+		    unset( $meta['download_url'] );
+        }
+		return $meta;
+	}
+    protected function actionDownload(Wpup_Request $request) {
+        if( !softupdatemgr()->isAuthenticatedRequest() ){
+		    $this->exitWithError('Downloads are disabled.', 403);
+        }
+	}
 }
--- class/softupdatemgr.class.php
+++ class/softupdatemgr.class.php
@@ -1,7 +1,14 @@
 <?php
+/*
+ * Base on https://github.com/YahnisElsts/wp-update-server;
+ * URL Exemple: https://dev.jony.co/siscafelicense?softupdatemgr_action=get_metadata&softupdatemgr_slug=siscafe
+ */
 class softupdatemgr{
     protected $updateServer;
-    
+	protected $licenseValidationMethods = array(
+		'no-validation' => '__return_true',
+	);
+
     public function __construct(){
         require_once SOFTUPDATEMGR_DIR_PATH . 'class/softupdatemgr-server.class.php';
         $this->updateServer = new softupdatemgr_server( home_url( '/' ) );
@@ -9,16 +16,21 @@ class softupdatemgr{
 		//or other plugins, so lets use different parameter names to avoid conflict.
 		add_filter( 'query_vars', array( $this, 'addQueryVariables' ) );
 		add_action( 'template_redirect', array( $this, 'handleUpdateApiRequest' ) );
+
+		// Adds License Validation Methods Filter
+		add_action( 'plugins_loaded', array( $this, 'applyLicenseValidationMethodsFilter' ) );
+		add_filter( 'softupdatemgr/add_license_validation_method', array( $this, 'addLicenseValidationMethods' ) );
     }
-    
+
     public function addQueryVariables($queryVariables) {
 		$queryVariables = array_merge($queryVariables, array(
 			'softupdatemgr_action',
 			'softupdatemgr_slug',
+			'softupdatemgr_license'
 		));
 		return $queryVariables;
 	}
-	
+
 	public function handleUpdateApiRequest() {
 		if ( get_query_var( 'softupdatemgr_action' ) ) {
 			$this->updateServer->handleRequest( array_merge($_GET, array(
@@ -26,5 +38,32 @@ class softupdatemgr{
 				'slug'   => get_query_var('softupdatemgr_slug'),
 			) ) );
 		}
+	}
+
+	public function addLicenseValidationMethods( $methods ){
+		$is_plugin_active = true;
+		if( $is_plugin_active ){
+			$methods['software-license-manager'] = array( $this, 'validateSLMLicense' );
+		}
+		return $methods;
+	}
+
+	public function applyLicenseValidationMethodsFilter(){
+		$this->licenseValidationMethods = apply_filters( 'softupdatemgr/add_license_validation_method', $this->licenseValidationMethods );
+	}
+
+	public function isValidLicense( $license ){
+		$validationMethod = 'no-validation';
+		$validationMethod = $this->licenseValidationMethods[$validationMethod];
+		return call_user_func( $validationMethod, $license );
+	}
+
+	public function isAuthenticatedRequest(){
+		$license = get_query_var('softupdatemgr_license');
+		return $this->isValidLicense( $license );
+	}
+
+	public function validateSLMLicense( $license ){
+		return true;
 	}
 }
